a63242ec3f10aa08bd9526706f88baa415e97c70
Kurt Petrek <kurtpetrek@gmail.com>
2017-04-22 23:23:25 -0400

    add better flower spacing and z-index

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
-function getRandomArbitrary(min, max) {
-  return Math.round(Math.random() * (max - min)) + min;
-}
+(function() {
 
-let flwrNum = getRandomArbitrary(10, 20);
+  function getRandomArbitrary(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+  }
+  
+  let positions = [];
+  
+  function getNum(){
+    let pos = getRandomArbitrary(0, 100);
+    for(let x = 0; x < positions.length; x++){
+      if(pos > positions[x] - 3 && pos < positions[x] + 3){
+        return false;
+      }
+    }
+    positions.push(pos);
+  }
+  
+  while(positions.length < 20){
+    getNum();
+  }
+  
+  console.log(positions);
 
-let more = setInterval(function(){
-  let flwr = document.createElement('div');
-  let dim = getRandomArbitrary(40, 70);
-  flwr.classList.add('sun-flwr');
-  flwr.innerHTML = `<div class="sun-flwr__leaf--left"></div>
+  let more = setInterval(function() {
+    let flwr = document.createElement('div');
+    let dim = getRandomArbitrary(30, 80);
+    let leftPos = positions[0];
+    positions.shift();
+    
+    flwr.classList.add('sun-flwr');
+    flwr.innerHTML = `<div class="sun-flwr__leaf--left"></div>
   <div class="sun-flwr__leaf--right"></div>
   <div class="sun-flwr__stem"></div>
   <div class="sun-flwr__center"></div>
@@ -24,12 +45,14 @@ let more = setInterval(function(){
   <div class="sun-flwr__pedal--10"></div>
   <div class="sun-flwr__pedal--11"></div>
   <div class="sun-flwr__pedal--12"></div>`;
-  flwr.style.left = getRandomArbitrary(0, 100) + "vw";
-  flwr.style.height = dim + "vmin";
-  flwr.style.width = dim + "vmin";
-  document.querySelector('body').appendChild(flwr);
-}, 800);
+    flwr.style.left = leftPos + "vw";
+    flwr.style.height = dim + "vmin";
+    flwr.style.width = dim + "vmin";
+    flwr.style.zIndex = 100 - dim;
+    document.querySelector('body').appendChild(flwr);
+  }, 150);
 
-setTimeout(function(){
-  window.clearInterval(more);
-}, 16000);
+  setTimeout(function() {
+    window.clearInterval(more);
+  }, 3000);
+})();
