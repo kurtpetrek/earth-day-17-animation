34deff5b198e875acf275b3cfc5bed747028f130
Kurt Petrek <kurtpetrek@gmail.com>
2017-05-08 20:49:09 -0400

    added random color change for random flowers

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 (function() {
+  
+  let positions = [];
 
   function getRandomArbitrary(min, max) {
     return Math.round(Math.random() * (max - min)) + min;
   }
-  
-  let positions = [];
   
   function getNum(){
     let pos = getRandomArbitrary(0, 100);
@@ -19,7 +19,7 @@
   while(positions.length < 20){
     getNum();
   }
-  
+
   let more = setInterval(function() {
     let flwr = document.createElement('div');
     let dim = getRandomArbitrary(30, 80);
@@ -28,25 +28,26 @@
     
     flwr.classList.add('sun-flwr');
     flwr.innerHTML = `<div class="sun-flwr__leaf--left"></div>
-  <div class="sun-flwr__leaf--right"></div>
-  <div class="sun-flwr__stem"></div>
-  <div class="sun-flwr__center"></div>
-  <div class="sun-flwr__pedal--1"></div>
-  <div class="sun-flwr__pedal--2"></div>
-  <div class="sun-flwr__pedal--3"></div>
-  <div class="sun-flwr__pedal--4"></div>
-  <div class="sun-flwr__pedal--5"></div>
-  <div class="sun-flwr__pedal--6"></div>
-  <div class="sun-flwr__pedal--7"></div>
-  <div class="sun-flwr__pedal--8"></div>
-  <div class="sun-flwr__pedal--9"></div>
-  <div class="sun-flwr__pedal--10"></div>
-  <div class="sun-flwr__pedal--11"></div>
-  <div class="sun-flwr__pedal--12"></div>`;
-    flwr.style.left = leftPos + "vw";
-    flwr.style.height = dim + "vmin";
-    flwr.style.width = dim + "vmin";
+                      <div class="sun-flwr__leaf--right"></div>
+                      <div class="sun-flwr__stem"></div>
+                      <div class="sun-flwr__center"></div>
+                      <div class="sun-flwr__pedal--1"></div>
+                      <div class="sun-flwr__pedal--2"></div>
+                      <div class="sun-flwr__pedal--3"></div>
+                      <div class="sun-flwr__pedal--4"></div>
+                      <div class="sun-flwr__pedal--5"></div>
+                      <div class="sun-flwr__pedal--6"></div>
+                      <div class="sun-flwr__pedal--7"></div>
+                      <div class="sun-flwr__pedal--8"></div>
+                      <div class="sun-flwr__pedal--9"></div>
+                      <div class="sun-flwr__pedal--10"></div>
+                      <div class="sun-flwr__pedal--11"></div>
+                      <div class="sun-flwr__pedal--12"></div>`;
+    flwr.style.left = `${leftPos}vw`;
+    flwr.style.height = `${dim}vmin`;
+    flwr.style.width = `${dim}vmin`;
     flwr.style.zIndex = 100 - dim;
+    flwr.style.filter = `saturate(${getRandomArbitrary(70, 100)}%) brightness(${getRandomArbitrary(80, 150)}%)`;
     document.querySelector('body').appendChild(flwr);
   }, 150);
 
